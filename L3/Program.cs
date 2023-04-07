1f6983d983cf544fa98a3f1e5bd42606cae6de8f
NguyenTin2104 <hongtin2104@gmail.com>
2023-04-07 10:28:05 +0700

    currency converter app

---
+++ L3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace L3{
+    class Program{
+        const float VND = 23000; 
+        static void Main(){
+            double USD;
+            Console.Write("Enter USD: ");
+            USD = float.Parse((Console.ReadLine()) ?? "0");
+            Console.WriteLine($"{USD} USD = {USD * VND} VND");
+            
+        }
+    }
+}
