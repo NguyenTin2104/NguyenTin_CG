1b7817313790d44b8bfc777803f7581fe5bdd3f3
NguyenTin2104 <hongtin2104@gmail.com>
2023-04-07 10:15:24 +0700

    L2

---
+++ L2/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Hello{
+    class Program{
+        static void Main(){
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine() ?? "0";
+            Console.WriteLine("Hello: " + name);
+
+        }
+    }
+}
