9503397e3e76f844549b80c1ac9c57f855657155
NguyenTin2104 <hongtin2104@gmail.com>
2023-04-07 09:43:26 +0700

    L1

---
+++ L1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace L1{
+    class Program{
+        static void Main(){
+        float width, height;
+        Console.Write("Enter width: ");
+        width = float.Parse(Console.ReadLine());
+        Console.Write("Enter height: ");
+        height = float.Parse(Console.ReadLine());
+        Console.WriteLine("Area is: " + (width*height));
+        }
+    }
+}
