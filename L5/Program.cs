3f6553a5e1065ae938226a1d62bc09ad01f86c81
NguyenHongTin <hongtin2104@gmail.com>
2023-04-08 08:40:17 +0700

    BMI calculator

---
+++ L5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace L5
+{
+    class IBM
+    {
+        static void Main()
+        {
+            double height;
+            double weight;
+            Console.Write("Please enter your height: ");
+            height = double.Parse((Console.ReadLine()) ?? "0");
+            Console.Write("Please enter your weight: ");
+            weight = double.Parse((Console.ReadLine()) ?? "0");
+            double bmi = weight / Math.Pow(height, 2);
+            bmi = Math.Round(bmi, 1);
+            Console.Write("BMI: " + bmi);
+            if (bmi < 18)
+                Console.Write(" Underweight");
+            else if (bmi < 25.0)
+                Console.WriteLine(" Normal");
+            else if (bmi < 30.0)
+                Console.WriteLine(" Overweight");
+            else
+                Console.WriteLine(" Obese");
+        }
+    }
+}
