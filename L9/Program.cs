9f697611d91af957163b486642ff594f8a8ec07b
NguyenHongTin <hongtin2104@gmail.com>
2023-04-11 16:21:01 +0700

    Is prime number (while)

---
+++ L9/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace L9{
+    class Program{
+        static void Main(){
+            Console.Write("Enter n: ");
+            int n;
+            n = int.Parse((Console.ReadLine()) ?? "\0");
+            int key = n - 1;
+            bool flag = false;
+            while(n >= 2){
+                if(key == 1){
+                    flag = true;
+                    break;
+                }
+                if(n == 2){
+                    flag = true;
+                    break;
+                }
+                if(n % key == 0){
+                    flag = false;
+                    break;
+                }
+                else{
+                    key--;
+                }
+                
+            }
+            if(flag){
+                Console.Write($"{n} is prime number!");
+            }
+            else{
+                Console.Write($"{n} is not prime number!");
+            }
+        }
+    }
+}
