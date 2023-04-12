6604cbb7971a4d532aef70cdc9331c2b1e0c2b0f
NguyenHongTin <hongtin2104@gmail.com>
2023-04-12 15:15:31 +0700

    Sum arr

---
+++ L12/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace L12
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] a = new int[5];
+            int sum =0;
+            for (int i = 0; i < 5; i++)
+            {
+                a[i] = int.Parse(Console.ReadLine() ?? "0");
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                sum += a[i];
+            }
+            Console.Write("Sum of array: " + sum);
+        }
+    }
+}
