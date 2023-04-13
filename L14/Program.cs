7f85d391658ffaa23cb4aecab42d7ac871326385
NguyenHongTin <hongtin2104@gmail.com>
2023-04-13 14:11:06 +0700

    Sum of the main diagonal of a 2D array

---
+++ L14/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace SumOfTheMainDiagonalOfA2DArray
+{
+    class Program
+    {
+        static void Main()
+        {
+            int row, col;
+            Console.Write("Enter rows: ");
+            row = int.Parse(Console.ReadLine() ?? "0");
+            Console.Write("Enter rows: ");
+            col = int.Parse(Console.ReadLine() ?? "0");
+            double[,] a = new double[row, col];
+            double sum = 0;
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    a[i, j] = double.Parse(Console.ReadLine() ?? "0");
+                    if(i == j){
+                        sum += a[i,j];
+                    }
+                }
+
+            }
+            Console.WriteLine("Sum of the main diagonal of a 2D array: " + sum);
+        }
+    }
+}
