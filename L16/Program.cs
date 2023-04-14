16fc92e5db6b0890c8c9467715de1c08eb175b37
NguyenHongTin <hongtin2104@gmail.com>
2023-04-14 13:13:19 +0700

    Count the number of occurrences of a character in a string (func)

---
+++ L16/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace L16
+{
+    class Program
+    {
+        public void Count(string a, char x)
+        {
+            int count = 0;
+
+                for (int i = 0; i < a.Length; i++)
+                {
+                    if (x == a[i])
+                    {
+                        count++;
+                    }
+                }
+                Console.Write($"Element {x} appears {count} times");
+        }
+        static void Main()
+        {
+            string a = "Hello CodeGymmm!";
+            Console.Write("Enter the element to count the number of occurrences: ");
+            char x = char.Parse(Console.ReadLine() ?? "\0");
+            Program p = new Program(); 
+            p.Count(a, x);
+        }
+    }
+}
